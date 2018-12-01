2b644878d0b067547c5117dfdce99b8ea66e47b5
Danni B <danni.ballena@gmail.com>
2018-11-30 20:04:26 -0800

    added db connection and router

--- src/server/db.js
+++ src/server/db.js
@@ -1,15 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config()
 
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test');
 
-const connection = process.env.MONGO_URI;
+mongoose.connect(process.env.MONGO_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
   console.log('connected to MONGO DB!')
 });
 
